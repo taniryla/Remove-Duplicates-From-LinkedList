c632e6c83aa8459022377b6cbc00b5400a248d6b
Ryland Taniguchi <ryland0000@gmail.com>
2022-03-29 01:06:15 -0700

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+// This is an input class. Do not edit.
+class LinkedList {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
+function removeDuplicatesFromLinkedList(linkedList) {
+  let currentNode = linkedList;
+  while (currentNode !== null) {
+    let nextDistinctNode = currentNode.next;
+    while (
+      nextDistinctNode !== null &&
+      nextDistinctNode.value === currentNode.value
+    ) {
+      nextDistinctNode = nextDistinctNode.next;
+    }
+
+    currentNode.next = nextDistinctNode;
+    currentNode = nextDistinctNode;
+  }
+
+  return linkedList;
+}
